64debae3b6a41f05456046aa78a3162d3c91b77c
Sylvie <sylvie.z.lu@gmail.com>
2020-09-28 11:32:52 +0200

    eslint prettier config added

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const connection = require('./config/db.config')
+const connection = require('./config/db.config');
 
 const server = express();
 
 server.use(cors());
 
 server.get('/', (req, res) => {
-    return res.send('Home')
+  return res.send('Home');
 });
 
 server.get('/products', (req, res) => {
-  connection.query(
-    'SELECT * FROM `products`', function(err, results, fields) {
-      res.json(results)
-    }
-  )
-})
+  connection.query('SELECT * FROM `products`', function (err, results, fields) {
+    res.json(results);
+  });
+});
 
 module.exports = server;
