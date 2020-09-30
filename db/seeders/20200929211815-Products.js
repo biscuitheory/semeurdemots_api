9eb7f8639fa3c6780c656f9e227e92847a189fe6
Sylvie <sylvie.z.lu@gmail.com>
2020-09-30 10:36:12 +0200

    seeder products w/ productType ok

--- db/seeders/20200929211815-Products.js
+++ db/seeders/20200929211815-Products.js
@@ -1,19 +1,20 @@
 const faker = require('faker');
 
-const productType = ['book', 'produit dérivé'];
-const randomProductType =
-  productType[Math.floor(Math.random() * productType.length)];
-
-const products = [...Array(5)].map((product) => ({
-  name: faker.commerce.productName(),
-  type: randomProductType,
-  price: faker.commerce.price(),
-  stock: faker.random.number(),
-  description: faker.commerce.productDescription(),
-  image: faker.image.cats(),
-  createdAt: new Date(),
-  updatedAt: new Date(),
-}));
+const products = [...Array(5)].map((product) => {
+  const productType = ['book', 'produit dérivé'];
+  const randomProductType =
+    productType[Math.floor(Math.random() * productType.length)];
+  return {
+    name: faker.commerce.productName(),
+    type: randomProductType,
+    price: faker.commerce.price(),
+    stock: faker.random.number(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.cats(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+});
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
