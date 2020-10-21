b621c5dc1883eda32b9919c70550cd3f4b58c15a
Sylvie <sylvie.z.lu@gmail.com>
2020-10-21 21:33:11 +0200

    clean code delete comments products controller

--- controllers/products.js
+++ controllers/products.js
@@ -49,32 +49,9 @@ module.exports = {
   getProductFromCart: async (data) => {
     const products = await Product.findAll();
     const filteredProducts = [];
-    const { keys, values } = data;
 
     console.log('touc', data);
 
-    // products.forEach((product) => {
-    //   data.keys.forEach((key) => {
-    //     if (key == product.id) {
-    //       filteredProducts.push(product);
-    //       // filteredProducts.push({
-    //       //   didi: key,
-    //       //   ...product,
-    //       //   quantity: data.values,
-    //       // });
-
-    //       // filteredProducts.push({ ...product, quantity: values });
-    //     }
-    //   });
-    // });
-
-    // const filteredProducts = Object.entries(data.localStorage).map((item) =>
-    //   console.log('zaza', item)
-    // );
-
-    // for (let key in data.localStorage) {
-    //   console.log('tata', key, data.localStorage[key])
-    // }
     products.forEach((product) => {
       for (let [key, value] of Object.entries(data.localStorage)) {
         // console.log('papi', key, value)
