e58f17f38375acd668568401a226b1ff8654f75a
Sylvie <sylvie.z.lu@gmail.com>
2020-11-17 02:44:50 +0100

    test get users

---
+++ controllers/users.spec.js
@@ -0,0 +1,25 @@
+// const chai = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const should = chai.should();
+const db = require('../models');
+
+const { User } = db;
+
+const server = require('../server.js');
+
+chai.use(chaiHttp);
+describe('/GET users', () => {
+  it('it should GET all the users', (done) => {
+    chai
+      // .request(server)
+      .request('http://localhost:8088/api')
+      .get('/users')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        done();
+      });
+  });
+});
